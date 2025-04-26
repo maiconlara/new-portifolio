231972e0b11e31ae34fae99942f316366bd93218
Maicon <maiconluan.lara@yahoo.com.br>
2025-04-26 17:44:32 -0300

    feat: header

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,50 @@
+import icon from "@/assets/icon.svg";
+import { RiChat1Line, RiMailSendLine } from "@remixicon/react";
+import Image from "next/image";
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header className="bg-black flex flex-row h-18 w-full items-center justify-center border-b border-lines">
+      <div className="bg-black flex flex-row h-full w-full max-w-[1200px] items-center justify-between px-4 ">
+        <div className="flex flex-row gap-2 items-center select-none">
+          <Image
+            width={44}
+            height={44}
+            alt="M"
+            className="h-auto w-[30px]"
+            src={icon}
+            quality={100}
+            priority={true}
+          />
+          <p className="font-title text-white text-xl font-semibold">
+            Maicon Lara
+          </p>
+        </div>
+        <div className="flex flex-row gap-4 items-center">
+          <Link
+            className="text-white font-text text-xl hover:text-purple-light transition-colors"
+            href="/"
+          >
+            Inicio
+          </Link>
+          <Link
+            className="text-white font-text text-xl hover:text-purple-light transition-colors"
+            href="/"
+          >
+            Sobre
+          </Link>
+          <Link
+            className="text-white font-text text-xl hover:text-purple-light transition-colors"
+            href="/"
+          >
+            Projetos
+          </Link>
+          <div className="flex flex-row gap-2 items-center justify-center rounded-full w-10  h-10 cursor-pointer text-base font-text font-medium border border-lines transition-colors">
+            <RiMailSendLine size={16} className="text-white" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
